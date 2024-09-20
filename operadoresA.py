eecde7371cfc091ea97f51e0112c208ae2e74ac5
AlexisJimenez14 <a.22308051281213@cbtis128.edu.mx>
2024-09-20 09:38:01 -0600

    Add files via upload

---
+++ operadoresA.py
@@ -0,0 +1,73 @@
+print("--Actividad 9 clases version 3--")
+print("Alexis Fabian matricula:22308051281213")
+# zona de clases
+
+class Operadores1213:
+    # zona de funcion------------------------
+#zona suma
+    def suma1213(self,A,F):
+        s1213=A+F
+        print(f"La suma de {A} + {F} = {s1213}")
+    
+#zona resta
+    def resta1213(self,A,F):
+        r1213=A-F
+        print(f"La resta de {A} - {F} = {r1213}")
+
+#zona multiplicacion
+    def multi1213(self,A,F):
+        m1213=A*F
+        print(f"La multiplicacion de {A} * {F} = {m1213}")
+
+#zona multiplicacion
+    def divi1213(self,A,F):
+        d1213=A/F
+        print(f"La division de {A} / {F} = {d1213}")
+
+#zona modulo
+    def mod1213(self,A,F):
+        mo1213=A%F
+        print(f"el modulo de {A} % {F} = {mo1213}")
+
+#zona exponente
+    def expo1213(self,A,F):
+        e1213=A**F
+        print(f"El exponente de {A} ** {F} = {e1213}")
+
+#zona cociente
+    def cociente1213(self,A,F):
+        c1213=A//F
+        print(f"El cociente de {A} // {F} = {c1213}")
+
+# creacion del objeto ---------------------
+
+operaALEXIS=Operadores1213()
+#zona de uso ede objeto -----------------------
+#suma
+print("--funcion suma--")
+operaALEXIS.suma1213(10,4)
+
+#resta
+print("--funcion resta--")
+operaALEXIS.resta1213(10,4)
+
+#multiplicacion
+print("--funcion multiplicacion--")
+operaALEXIS.multi1213(2,4)
+
+#division
+print("--funcion division--")
+operaALEXIS.divi1213(10,2)
+
+#modulo
+print("--funcion modulo--")
+operaALEXIS.mod1213(1,4)
+
+#exponente
+print("--funcion exponente--")
+operaALEXIS.expo1213(2,4)
+
+#cociente
+print("--funcion cociente--")
+operaALEXIS.cociente1213(5,4)
+
